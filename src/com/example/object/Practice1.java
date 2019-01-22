c569e480b2cf6e7465e225db8692c10feec96724
zhangying <zhangying.thunisoft.com>
2019-01-22 13:40:54 +0800

    重新提交

---
+++ src/com/example/object/Practice1.java
@@ -0,0 +1,14 @@
+package com.example.object;
+
+public class Practice1 {
+
+    static int i;
+    static char c;
+    static String s;
+
+    public static void main(String[] args) {
+        System.out.println("int = " + i);
+        System.out.println("char = " + c);
+        System.out.println("String = " + s);
+    }
+}
